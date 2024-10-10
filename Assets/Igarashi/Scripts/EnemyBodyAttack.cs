33930c4a628be79ff4a2e80cfdb1a51f9d8c599a
waka822 <waka.igarashi@live.jp>
2024-10-11 04:19:45 +0900

    一面完成？

---
+++ Assets/Igarashi/Scripts/EnemyBodyAttack.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBodyAttack : MonoBehaviour
+{
+    private Enemy_base enemy_base;
+
+    void Start()
+    {
+        enemy_base = GetComponentInParent<Enemy_base> ();
+
+        var Coll_TouchArea = GetComponent<BoxCollider2D> ();
+        var Coll_body = enemy_base.gameObject.GetComponent<BoxCollider2D> ();
+        Coll_TouchArea.offset = Coll_body.offset;
+        Coll_TouchArea.size =Coll_body.size;
+        Coll_TouchArea.size *= 0.8f;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        string tag = collision.gameObject.tag;
+
+        if (tag == "Shuyaku")
+        {
+            enemy_base.BodyAttack (collision.gameObject);
+        }
+    }
+}
